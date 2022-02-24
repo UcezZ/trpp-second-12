1b51c8646c396b18fba282e4f45b8deaeef25119
UcezZ <ucezz@mail.ru>
2022-02-24 18:17:09 +0300

    fixed package name

--- src/main/java/ru/mirea/trppsecond12/controllers/AnimalController.java
+++ src/main/java/ru/mirea/trppsecond12/controllers/AnimalController.java
@@ -1,10 +1,10 @@
-package ru.mirea.trpp_second_12.controllers;
+package ru.mirea.trppsecond12.controllers;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import ru.mirea.trpp_second_12.entity.Animal;
+import ru.mirea.trppsecond12.entity.Animal;
 
 import java.io.InputStreamReader;
 import java.util.List;
--- src/main/java/ru/mirea/trppsecond12/controllers/HealthController.java
+++ src/main/java/ru/mirea/trppsecond12/controllers/HealthController.java
@@ -1,9 +1,9 @@
-package ru.mirea.trpp_second_12.controllers;
+package ru.mirea.trppsecond12.controllers;
 
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import ru.mirea.trpp_second_12.entity.HealthResponse;
+import ru.mirea.trppsecond12.entity.HealthResponse;
 
 /** Проверка состояния сервера. */
 @Controller()
@@ -11,8 +11,9 @@ public class HealthController {
 
     /**
      * Проверить состояние сервера.
+     * 
      * @return ответ
-     * 200 - OK
+     *         200 - OK
      */
     @Get
     public HttpResponse<HealthResponse> healthCheck() {
